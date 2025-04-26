8bd86074576d374570155d2a8d6f535fcb491c92
Trần Trọng Khang <23DH111538@st.huflit.edu.vn>
2025-04-26 09:37:43 +0700

    Add project files.

---
+++ WebHackathon/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebHackathon.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebHackathon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebHackathon.WebApiApplication" Language="C#" %>
